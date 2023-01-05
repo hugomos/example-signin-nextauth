1381a0166d1a977566da73cfefe2fce2c6c40ea7
Vitor <vitor_osantos@hotmail.com>
2023-01-05 11:44:43 -0300

    config: alterando basePath next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: 'https://signin-auth.vercel.app/',
+  basePath: '/',
 }
 
 module.exports = nextConfig
